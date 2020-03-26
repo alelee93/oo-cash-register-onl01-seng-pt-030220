9a444d9aff6d2dfb0f4f6f1bc0bed6cc0d709a6d
Alejandra I. Lee Vargas <leevargasale@gmail.com>
2020-03-26 02:48:40 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
 
+class CashRegister
+
+    attr_accessor :total :discount :quantity
+
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+    end
+
+    def total
+        @total
+    end
+
+    def add_item(title, price, quantity=0)
+        self.total += price 
+    end
+end
